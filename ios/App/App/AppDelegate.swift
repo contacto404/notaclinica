df93c6602cc14c4af45659668ad34520a0dda2bd
contacto404 <contacto@vibraco.com.uy>
2026-05-31 14:58:18 -0300

    fix: touch-action pan-y bloquear scroll horizontal Capacitor

--- ios/App/App/AppDelegate.swift
+++ ios/App/App/AppDelegate.swift
@@ -1,5 +1,6 @@
 import UIKit
 import Capacitor
+import WebKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -7,43 +8,63 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
         return true
     }
 
-    func applicationWillResignActive(_ application: UIApplication) {
-        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-        // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
-    }
-
-    func applicationDidEnterBackground(_ application: UIApplication) {
-        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-    }
-
-    func applicationWillEnterForeground(_ application: UIApplication) {
-        // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
-    }
+    func applicationWillResignActive(_ application: UIApplication) {}
+    func applicationDidEnterBackground(_ application: UIApplication) {}
+    func applicationWillEnterForeground(_ application: UIApplication) {}
 
     func applicationDidBecomeActive(_ application: UIApplication) {
-        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
+            self.fixWebViewWidth()
+        }
     }
 
-    func applicationWillTerminate(_ application: UIApplication) {
-        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
-    }
+    func applicationWillTerminate(_ application: UIApplication) {}
 
     func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey: Any] = [:]) -> Bool {
-        // Called when the app was launched with a url. Feel free to add additional processing here,
-        // but if you want the App API to support tracking app url opens, make sure to keep this call
         return ApplicationDelegateProxy.shared.application(app, open: url, options: options)
     }
 
     func application(_ application: UIApplication, continue userActivity: NSUserActivity, restorationHandler: @escaping ([UIUserActivityRestoring]?) -> Void) -> Bool {
-        // Called when the app was launched with an activity, including Universal Links.
-        // Feel free to add additional processing here, but if you want the App API to support
-        // tracking app url opens, make sure to keep this call
         return ApplicationDelegateProxy.shared.application(application, continue: userActivity, restorationHandler: restorationHandler)
     }
 
+    private func fixWebViewWidth() {
+        guard let webView = findWebView(in: self.window?.rootViewController) else { return }
+
+        // Forzar el ancho correcto del WebView
+        let screenWidth = UIScreen.main.bounds.width
+        var frame = webView.frame
+        frame.size.width = screenWidth
+        webView.frame = frame
+
+        // Inyectar CSS para prevenir overflow
+        let js = """
+            (function() {
+                var style = document.createElement('style');
+                style.innerHTML = 'html, body { overflow-x: hidden !important; max-width: 100vw !important; width: 100% !important; }';
+                document.head.appendChild(style);
+            })();
+        """
+        webView.evaluateJavaScript(js, completionHandler: nil)
+    }
+
+    private func findWebView(in viewController: UIViewController?) -> WKWebView? {
+        guard let vc = viewController else { return nil }
+        if let found = findWebViewInView(vc.view) { return found }
+        for child in vc.children {
+            if let found = findWebView(in: child) { return found }
+        }
+        return nil
+    }
+
+    private func findWebViewInView(_ view: UIView) -> WKWebView? {
+        if let webView = view as? WKWebView { return webView }
+        for subview in view.subviews {
+            if let found = findWebViewInView(subview) { return found }
+        }
+        return nil
+    }
 }
